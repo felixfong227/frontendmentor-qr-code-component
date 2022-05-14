19648b552f4cd59365f72ac4ba0cb7578a287ad9
FeliΞ <moongod101@hotmail.com>
2022-05-15 05:37:35 +0800

    Initial commit

---
+++ tailwindcss.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                outfit: [
+                    'outfit'
+                ]
+            },
+            colors: {
+                white: '#FFFFFF',
+                lightGrey: '#D6E2F0',
+                greyishBlue: '#7B879D',
+                darkBlue: '#1F3251',
+            }
+        }
+    }
+}
